6bda43188717d350e313dbc798674576abd4ca92
mlizama <lizamamoises33@gmail.com>
2016-03-19 22:54:54 -0700

    did a small update

--- gpl/p5/expression.h
+++ gpl/p5/expression.h
@@ -3,12 +3,25 @@
 
 #include "expression.h"
 #include "gpl_type.h"
+#include <iostream>
+#include "variable.h"
+using namespace std;
 
 class Expression
 {
 	private:
+	enum Tree_kinds {EXPRESSION, CONSTANT, VARIABLE};
+	Tree_kinds m_kind;
+	Expression *leftxp;
+	Expression *rightxp;
 
 	public:
-	Expression(Expression *left,Gpl_type type, Expression *right);
+	Expression(Expression *left, Operator_type type, Expression *right);
+	Expression(Expression *exp, Operator_type type);
+        Expression(int i_value ,Gpl_type type);
+	Expression(double d_value, Gpl_type type);
+        Expression(string value, Gpl_type type);
+	Expression(Variable *variable);
+
 };
 #endif
--- gpl/p5/variable.cpp
+++ gpl/p5/variable.cpp
@@ -1,3 +1,3 @@
-
+#include "variable.h"
 
 
