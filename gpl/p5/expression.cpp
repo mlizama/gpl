ac1d35522aa96d3f6ada0460c08b8594efa040ff
mlizama <lizamamoises33@gmail.com>
2016-03-21 02:03:48 -0700

    added to expression

--- gpl/p5/expression.cpp
+++ gpl/p5/expression.cpp
@@ -1,1 +1,12 @@
 #include "expression.h"
+
+Expression::Expression(int i_value)
+{
+	int_value = i_value;
+	leftxp = NULL;
+	rightxp = NULL;
+	m_type = INT;
+	m_variable = NULL;
+	//ignore m_oper
+
+}
